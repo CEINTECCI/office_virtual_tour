c5bbb1cf39f8925f8073f356a3bed3dbc205cb4b
J4ngel <andres_angel@live.com.ar>
2023-03-22 12:41:15 -0500

    Carga inicial de los archivos requeridos

---
+++ data.js
@@ -0,0 +1,460 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-entrada-y-pasillo",
+      "name": "Entrada y pasillo",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.1871727064541755,
+          "pitch": 0.5142946673220639,
+          "rotation": 0,
+          "target": "1-entrada-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-entrada-",
+      "name": "Entrada ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.26301712326587534,
+          "pitch": 0.8853736454426944,
+          "rotation": 0,
+          "target": "2-oficina-11"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-oficina-11",
+      "name": "Oficina 1(1)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.15671278489661233,
+          "pitch": 0.7822699482105104,
+          "rotation": 11.780972450961727,
+          "target": "3-oficina22"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-oficina22",
+      "name": "Oficina2(2)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.22907867579196584,
+          "pitch": 0.8154519043192927,
+          "rotation": 0,
+          "target": "4-oficina33"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-oficina33",
+      "name": "Oficina3(3)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.0072839072451245,
+          "pitch": 0.9622501941588837,
+          "rotation": 0,
+          "target": "5-oficina44"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-oficina44",
+      "name": "Oficina4(4)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8640045935507956,
+          "pitch": 0.8944909164023258,
+          "rotation": 5.497787143782138,
+          "target": "6-oficina55"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-oficina55",
+      "name": "Oficina5(5)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.21386783408142307,
+          "pitch": 0.919545015464946,
+          "rotation": 42.4115008234622,
+          "target": "7-oficina66"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-oficina66",
+      "name": "Oficina6(6)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.544043588630717,
+          "pitch": 0.8389956926005588,
+          "rotation": 1.5707963267948966,
+          "target": "8-oficina77"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-oficina77",
+      "name": "Oficina7(7)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.40567441275029736,
+          "pitch": 0.8718207159634499,
+          "rotation": 5.497787143782138,
+          "target": "9-oficina88"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-oficina88",
+      "name": "Oficina8(8)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.10856778848959436,
+          "pitch": 0.9313661837875848,
+          "rotation": 0,
+          "target": "10-oficina99"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-oficina99",
+      "name": "Oficina9(9)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.2588648834061207,
+          "pitch": 0.9698102466652063,
+          "rotation": 0,
+          "target": "11-salida"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-salida",
+      "name": "Salida",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Recorrido Quinto piso",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
